55b6137e6c96566e802610cca1eaa20bf4be6743
LuayAlshami <luayalshami136@gmail.com>
2025-12-24 17:04:18 +0300

    Create ShuntingYard.c

---
+++ Homeworke 03/ShuntingYard.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+void push(char x) {
+    stack[++top] = x;
+}
+
+char pop() {
+    return stack[top--];
+}
+
+char peek() {
+    return stack[top];
+}
+
+int isEmpty() {
+    return top == -1;
+}
+
+int precedence(char op) {
+    if (op == '+' || op == '-')
+        return 1;
+    if (op == '*' || op == '/')
+        return 2;
+    return 0;
+}
+
+void infixToPostfix(char infix[], char postfix[]) {
+    int i, k = 0;
+    char ch;
+
+    for (i = 0; infix[i] != '\0'; i++) {
+        ch = infix[i];
+
+        if (isalnum(ch)) {
+            postfix[k++] = ch;
+        }
+        else if (ch == '(') {
+            push(ch);
+        }
+        else if (ch == ')') {
+            while (!isEmpty() && peek() != '(') {
+                postfix[k++] = pop();
+            }
+            pop(); 
+        }
+    
+        else {
+            while (!isEmpty() && precedence(peek()) >= precedence(ch)) {
+                postfix[k++] = pop();
+            }
+            push(ch);
+        }
+    }
+
+
+    while (!isEmpty()) {
+        postfix[k++] = pop();
+    }
+
+    postfix[k] = '\0';
+}
+
+int main() {
+    char infix[MAX], postfix[MAX];
+
+    printf("Infix ifadeyi giriniz: ");
+    scanf("%s", infix);
+
+    infixToPostfix(infix, postfix);
+
+    printf("Postfix ifade: %s\n", postfix);
+
+    return 0;
+}
